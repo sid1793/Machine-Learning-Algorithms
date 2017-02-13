3ec2c5427561f72fb251b1edc83382696a2ca20b
Siddhesh Mhatre <sid1793@gmail.com>
2017-02-13 15:06:06 -0700

    add function to compute discounted returns from each state

--- PG/util.py
+++ PG/util.py
@@ -1,6 +1,7 @@
 import numpy as np
+from collections import defaultdict
 
-def run_episode(agent, env, max_episode_length, render = False):
+def run_episode(agent, env, max_episode_length=25, render=False):
     """
     Runs an episode for the environment and returns dictionary of observations,
     rewards and actions
@@ -10,7 +11,8 @@ def run_episode(agent, env, max_episode_length, render = False):
     rewards = []
     actions = []
     for i in range(max_episode_length):
-        a = agent.take_action(obs)
+        #a = agent.take_action(obs)
+        a = env.action_space.sample()
         obs, rew, done, _ = env.step(a)
         observations.append(obs)
         rewards.append(rew)
@@ -21,3 +23,23 @@ def run_episode(agent, env, max_episode_length, render = False):
             "rewards": rewards,
             "actions": actions
            }
+
+def discounted_returns(rewards, gamma = 0.9):
+    """
+    Returns a list of discounted rewards for an episode at each time step
+    discounted exponentially by factor gamma
+    """
+    returns = [0]
+    for idx, rew in enumerate(reversed(rewards)):
+        disc_ret = rew + returns[idx] * gamma
+        returns.append(disc_ret)
+    returns.reverse()
+    return returns[:-1]
+
+def compute_baseline(observations, returns):
+    """
+    Returns a dictionary of the average returns from each state that serves as
+    the baseline
+    """
+    # TO-DO - Implement approximator of value function
+    raise NotImplementedError
